dce6fbde73560fa20c5ab12c7ae7e724ea7fb38b
Anton Mu <34460584+muckelba@users.noreply.github.com>
2018-03-02 21:14:02 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # check_rocketmap
-a script to check the spawned pokemon of a Rocketmap DB
+a script to check the spawned Pokémon of a local RocketMap DB. Can be used with Nagios or Icinga. Supports performance-data.
+
+## Usage:
+
+``./check_rocketmap [-o <threshold value for OK>] [-w <threshold value for WARNING>] [-u <DB user>] [-p <DB password>] [-d <Database>] ``
---
+++ check_rocketmap.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+usage() { echo "Usage: $0 [-o <threshold value for OK>] [-w <threshold value for WARNING>] [-u <DB user>] [-p <DB password>] [-d <Database>]" 1>&2; exit 1; }
+
+while getopts ":o:w:u:p:d:" a; do
+    case "${a}" in
+        o)
+            o=${OPTARG}
+            ;;
+        w)
+            w=${OPTARG}
+            ;;
+        u)
+            u=${OPTARG}
+            ;;
+        p)
+            p=${OPTARG}
+            ;;
+        d)
+            d=${OPTARG}
+            ;;
+        *)
+           usage
+           ;;
+    esac
+done
+shift $((OPTIND-1))
+
+#query local Rocketmap DB
+pokemon=$(mysql -u ${u} -p${p} -D ${d} -N -B -e "select count(*) from pokemon where disappear_time > utc_timestamp()")
+output="Spawned pokemon: $pokemon | spawned_pokemon=$pokemon"
+
+if [ "$pokemon" -gt "${o}" ]
+then
+    echo "OK - $output"
+    exit 0
+elif [ "$pokemon" -gt "${w}" ]
+then
+    echo "WARNING - $output"
+    exit 1
+elif [ "$pokemon" -le "${w}" ]
+then
+    echo "CRITICAL - $output"
+    exit 2
+else
+echo "UNKNOWN - $output"
+exit 3
+fi
